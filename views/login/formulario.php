7c818b18fced51f2b2bbfd8a85fd81364d6fe8a5
Néstor <nespapu@gmail.com>
2025-12-02 19:39:57 +0100

    refactor: borrar vistas obsoletas login y plantilla legacy

--- views/login/formulario.php
+++
@@ -1,5 +0,0 @@
-<form action="/oopps/public/login/comprobar" method="post">
-    <input type="text" name="nombre" id="nombre" placeholder="Introducir nombre">
-    <input type="password" name="clave" id="clave" placeholder="Introducir clave">
-    <input type="submit" value="Entrar">
-</form>
