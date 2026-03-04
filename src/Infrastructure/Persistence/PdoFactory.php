5efe8a78dd5d8a8699672aa2284608d34067aa9a
Néstor <nespapu@gmail.com>
2026-03-04 13:18:01 +0100

    chore: disable PDO emulated prepared statements

--- src/Infrastructure/Persistence/PdoFactory.php
+++ src/Infrastructure/Persistence/PdoFactory.php
@@ -25,6 +25,7 @@ final class PdoFactory
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
             ]
         );
     }
